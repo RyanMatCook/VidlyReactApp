33d4405001fc65bfc793f489eea3e3a84e2efddb
Ryan Cook <mr.rmcook@gmail.com>
2018-10-01 19:24:03 +0200

    Finalise form validation on login. Add other pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
 import Movies from "./components/movies";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Customers from "./components/customers";
+import Rentals from "./components/Rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/NavBar";
+import MovieForm from "./components/MovieForm";
+import LoginForm from "./components/loginForm";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <Movies />;
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
   }
 }
 
